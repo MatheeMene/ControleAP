8219d1aadf91eee7252c47b5bbdfef96f102208e
Matheus Menegat <matheusmenegat@hotmail.com>
2019-03-12 13:45:25 -0300

    AGORA VAI

--- db/migrate/20190228132632_remove_cpf_from_client.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCpfFromClient < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :clients, :cpf, :integer
-  end
-end
--- db/migrate/20190228132700_remove_rg_from_client.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRgFromClient < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :clients, :rg, :integer
-  end
-end
